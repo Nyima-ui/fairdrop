eaeb46d1edbf245426709b0ae13670169d641535
Tenzin Nyima <ntenzin492@gmail.com>
2026-02-22 10:46:17 +0530

    add redirect to link for google auth

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -64,6 +64,9 @@ const AuthPage = () => {
 
       const { error } = await supabase.auth.signInWithOAuth({
         provider: "google",
+        options: {
+          redirectTo: `${window.location.origin}/auth/callback`,
+        },
       });
       if (error) throw error;
     } catch (error) {
